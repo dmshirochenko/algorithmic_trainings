ae123940acb22d1b122a07ff38cfac93375ffe9e
Dmitriy Shirochenko <d.shirochenko@criteo.com>
2024-03-19 12:52:39 +0100

    2 rectanles linter fix

--- yandex_algo_5_0/part_2/J. Два прямоугольника.py
+++ yandex_algo_5_0/part_2/J. Два прямоугольника.py
@@ -5,10 +5,12 @@ with open("input.txt", "r") as reader:
         chars = [char for char in reader.readline().strip()]
         grid.append(chars)
 
+
 def print_grid(grid):
     # Print the grid
     for row in grid:
-        print(' '.join(row))
+        print(" ".join(row))
+
 
 def adjust_rectangle(grid, top, left, initial_bottom, initial_right):
     for bottom in range(initial_bottom, top, -1):
@@ -26,6 +28,7 @@ def adjust_rectangle(grid, top, left, initial_bottom, initial_right):
 
     return False, initial_bottom, initial_right
 
+
 def adjust_rectangle_right_over_bottom(grid, top, left, initial_bottom, initial_right):
     for right in range(initial_right, left, -1):
         for bottom in range(initial_bottom, top, -1):
@@ -42,6 +45,7 @@ def adjust_rectangle_right_over_bottom(grid, top, left, initial_bottom, initial_
 
     return False, initial_bottom, initial_right
 
+
 def draw_rectangle(grid, row, col, char_to_draw, dirrection_priority):
     rectangle_size = 0
     right = col
@@ -50,7 +54,7 @@ def draw_rectangle(grid, row, col, char_to_draw, dirrection_priority):
         if grid[row][right] == "b":
             element_from_top = get_element_value(grid, row - 1, right)
             element_from_down = get_element_value(grid, row + 1, right)
-            if element_from_top == 'b' and element_from_down == 'b':
+            if element_from_top == "b" and element_from_down == "b":
                 break
         right += 1
 
@@ -59,15 +63,15 @@ def draw_rectangle(grid, row, col, char_to_draw, dirrection_priority):
         if grid[bottom][col] == "b":
             element_from_left = get_element_value(grid, bottom, col - 1)
             element_from_down = get_element_value(grid, bottom, col + 1)
-            if element_from_left == 'b' and element_from_down == 'b':
+            if element_from_left == "b" and element_from_down == "b":
                 break
         bottom += 1
-    
+
     if dirrection_priority == "bottom":
         valid, adjusted_bottom, adjusted_right = adjust_rectangle(grid, row, col, bottom, right)
     else:
         valid, adjusted_bottom, adjusted_right = adjust_rectangle_right_over_bottom(grid, row, col, bottom, right)
-    
+
     if valid:
         for r in range(row, adjusted_bottom):
             for c in range(col, adjusted_right):
@@ -75,6 +79,7 @@ def draw_rectangle(grid, row, col, char_to_draw, dirrection_priority):
                 grid[r][c] = char_to_draw
 
     return (rectangle_size, row, col, adjusted_bottom, adjusted_right, char_to_draw)
+
 
 def process_grid(grid, dirrection_priority):
     rectangles = []
@@ -89,7 +94,7 @@ def process_grid(grid, dirrection_priority):
                     char_to_draw = "a"
                 rectangle = draw_rectangle(grid, row, col, char_to_draw, dirrection_priority)
                 rectangles.append(rectangle)
-    
+
     return rectangles
 
 
@@ -100,15 +105,12 @@ def draw_second_rectangle(grid, row, col, adjusted_bottom, adjusted_right):
             bottom += 1
     else:
         bottom = row + 1
-    
+
     for r in range(row, bottom):
         grid[r][col] = "a"
 
+
 def check_num_of_separate_rectangeles(grid, rectangles):
-    for rectangle in rectangles:
-        _, _, _, _, _, char_to_draw = rectangle
-        if char_to_draw == "c":
-            return False
     if len(rectangles) == 0:
         return False
     if len(rectangles) > 2:
@@ -118,9 +120,10 @@ def check_num_of_separate_rectangeles(grid, rectangles):
     elif len(rectangles) == 1 and rectangles[0][0] == 1:
         return False
     elif len(rectangles) == 1:
-        size, row, col, adjusted_bottom, adjusted_right, char_to_draw  = rectangles[0]
+        size, row, col, adjusted_bottom, adjusted_right, char_to_draw = rectangles[0]
         draw_second_rectangle(grid, row, col, adjusted_bottom, adjusted_right)
         return True
+
 
 def get_element_value(grid, row, col):
     if 0 <= row < len(grid) and 0 <= col < len(grid[0]):
@@ -128,27 +131,28 @@ def get_element_value(grid, row, col):
     else:
         return None
 
-#direction bottom priority
+
+# direction bottom priority
 grid_bottom_priority = [row[:] for row in grid]
 rectangles = process_grid(grid_bottom_priority, "bottom")
 result = check_num_of_separate_rectangeles(grid_bottom_priority, rectangles)
 
 right_priority = False
 if not result:
-    #direction right priority
+    # direction right priority
     grid_right_priority = [row[:] for row in grid]
     rectangles = process_grid(grid_right_priority, "right")
     result = check_num_of_separate_rectangeles(grid_right_priority, rectangles)
     right_priority = True
 
 if result:
-    ans = 'YES' + '\n'
+    ans = "YES" + "\n"
     if right_priority:
-        grid_str = '\n'.join("".join(row) for row in grid_right_priority)
+        grid_str = "\n".join("".join(row) for row in grid_right_priority)
     else:
-        grid_str = '\n'.join("".join(row) for row in grid_bottom_priority)
+        grid_str = "\n".join("".join(row) for row in grid_bottom_priority)
 else:
-    ans = 'NO'
+    ans = "NO"
 
 # Writing to the file
 with open("output.txt", "w") as file:

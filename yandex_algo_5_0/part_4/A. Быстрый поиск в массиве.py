d0d8685e34f2f0b156d29cf2f9c717bcb5b174c8
Dmitriy Shirochenko <d.shirochenko@criteo.com>
2024-03-25 16:35:01 +0100

    sequence issue

---
+++ yandex_algo_5_0/part_4/A. Быстрый поиск в массиве.py
@@ -0,0 +1,48 @@
+with open("input.txt", "r") as reader:
+    N = int(reader.readline().strip())
+    nums = [int(num) for num in reader.readline().strip().split(" ")]
+    requests_lst = []
+    num_of_requests = int(reader.readline().strip())
+
+    for i in range(num_of_requests):
+        L, R = map(int, reader.readline().strip().split(" "))
+        requests_lst.append((L, R))
+
+def binary_search(checked_list, target, direction):
+    is_element_found = False
+
+    left, right = 0, len(checked_list) - 1
+    index = -1
+
+    while left <= right:
+        mid = (left + right) // 2
+        if direction == 'left':
+            if checked_list[mid] >= target:
+                right = mid - 1
+                index = mid
+            else:
+                left = mid + 1
+        elif direction == 'right':
+            if checked_list[mid] <= target:
+                left = mid + 1
+                index = mid
+            else:
+                right = mid - 1
+
+    return index
+
+if __name__ == "__main__":
+    ans_lst = []
+    #sort lst
+    nums.sort()
+
+    for L, R in requests_lst:
+        left_index = binary_search(nums, L, direction='left')
+        right_index = binary_search(nums, R, direction='right')
+        if left_index == -1 or right_index == -1 or left_index > right_index:
+            ans_lst.append(0)
+        else:
+           ans_lst.append((right_index + 1) - left_index) 
+           
+with open("output.txt", "w") as file:
+    file.write(" ".join(str(num) for num in ans_lst))
---
+++ yandex_algo_5_0/part_4/B. Одномерный морской бой.py
@@ -0,0 +1,21 @@
+with open("input.txt", "r") as reader:
+    N = int(reader.readline().strip())
+
+def find_total_num_of_squares(k):
+    return ((k * ((k**2) + (6*k) + 5))//6) - 1
+
+def binary_search(left, right, n):
+    while left < right:
+        mid_index = (left + right + 1) // 2
+
+        if find_total_num_of_squares(mid_index) <= n:
+            left = mid_index
+        else:
+            right = mid_index - 1
+             
+    return left
+
+ans = binary_search(1, N, N)
+
+with open("output.txt", "w") as file:
+    file.write(str(ans))

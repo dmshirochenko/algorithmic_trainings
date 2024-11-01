628da70bf0972cac14232b0dd1c9efc6fe3ba846
Dmitriy Shirochenko <d.shirochenko@criteo.com>
2024-11-01 22:27:20 +0100

    task 1

---
+++ yandex_algo_6_0/part_2/B. Сумма номеров.py
@@ -0,0 +1,10 @@
+with open('input.txt', 'r') as reader:
+    N, K = map(int, reader.readline().split())
+    nums = list(map(int, reader.readline().split()))
+
+def 
+
+
+ans = 1
+with open("output.txt", "w") as file:
+    file.write(str(ans))

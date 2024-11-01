9271ed7ed2e683ea709da3308f1347e39dad7d8b
Dmitriy Shirochenko <d.shirochenko@criteo.com>
2024-11-02 00:06:34 +0100

    task 2

--- yandex_algo_6_0/part_2/B. Сумма номеров.py
+++ yandex_algo_6_0/part_2/B. Сумма номеров.py
@@ -2,9 +2,20 @@ with open('input.txt', 'r') as reader:
     N, K = map(int, reader.readline().split())
     nums = list(map(int, reader.readline().split()))
 
-def 
+def find_length_of_equals_to_k(nums, k):
+    left = curr =  0
+    times_seen = 0
+    for right in range(len(nums)):
+        curr += nums[right]
+        while curr > k:
+            curr -= nums[left]
+            left += 1
+        if curr == k:
+            times_seen += 1
 
+    return times_seen
 
-ans = 1
+ans = find_length_of_equals_to_k(nums, K)
+
 with open("output.txt", "w") as file:
     file.write(str(ans))
